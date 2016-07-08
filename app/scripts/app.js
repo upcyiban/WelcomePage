efadbbb0effababad7a186c7f2b01f9c24f74da4
wulei123 <1534563365@qq.com>
2016-07-08 21:04:32 +0800

    yeoman项目生成

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name welcomePageApp
+ * @description
+ * # welcomePageApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('welcomePageApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name welcomePageApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the welcomePageApp
+ */
+angular.module('welcomePageApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

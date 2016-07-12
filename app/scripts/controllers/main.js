84717ba7efa0af8d26192cb37773b78c75036bcb
zhangfuli <1171067930@qq.com>
2016-07-12 20:00:33 +0800

    导航页

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,4 +14,49 @@ angular.module('welcomePageApp')
       'AngularJS',
       'Karma'
     ];
+
+  })
+  .controller('ShopListCtrl',function($scope) {
+    $scope.shops = [
+      {
+        'title': '基本查询',
+        'src':'#/search',
+        'imgsrc':'images/touxiang1.jpg'
+      },
+      {
+        'title': '开学事项',
+        'src':'#/about',
+        'imgsrc':'images/touxiang1.jpg'
+
+      }
+      ,
+      {
+        'title': '易班指南',
+        'src':'#/direct',
+        'imgsrc':'images/touxiang1.jpg'
+      }
+      ,
+      {
+        'title': '校园须知',
+        'src':'#/known',
+        'imgsrc':'images/touxiang1.jpg'
+      },
+      {
+        'title': '学校周边',
+        'src':'#/around',
+        'imgsrc':'images/touxiang1.jpg'
+      },
+      {
+        'title': '学子风采',
+        'src':'#/elegant',
+        'imgsrc':'images/touxiang1.jpg'
+      },
+      {
+        'title': '石大美景',
+        'src':'#/beauty',
+        'imgsrc':'images/touxiang1.jpg'
+      },
+    ]
   });
+
+
